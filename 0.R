e8032a152b3a731fb74e8a465601318978251a40
mmariangeles <mariangelesmz90@gmail.com>
2025-01-30 22:25:36 -0300

    301

---
+++ 0.R
@@ -0,0 +1,26 @@
+#esto es un .R que tiene como objetivo realizar source con otros. 
+
+#librerias
+library(readr)
+library(tidyverse)
+library(dplyr)
+library(knitr)
+library(ggplot2)
+library(readxl)
+library(lubridate)
+library(gridExtra)
+
+
+
+source("1_general.R")
+source("2_respiratorias.R")
+source("3_diarreas.R")
+source("4_lesiones_hogar.R")
+source("5_mordeduras.R")
+source("6_siniestros viales.R")
+source("7_sifilis.R")
+source("8_inmunoprevenibles.R")
+
+
+
+
